a6a490435011e368310feac6ea149e7ac02ef7f5
zhuangtianyu <zhuangtianyu1995@163.com>
2020-01-25 01:34:52 +0800

    feat: 文章详情接口

--- index.js
+++ index.js
@@ -19,15 +19,15 @@ const sleep = (ms = 1000) => new Promise((resolve) => {
   setTimeout(() => resolve(), ms)
 })
 
-router.get('/luck/article/list', async (ctx, next) => {
-  const service = () => new Promise((resolve, reject) => {
-    fs.readFile('./article-map.json', 'utf8', (error, data) => (
-      error !== null ? reject(error) : resolve(data)
-    ))
-  })
+const fetchArticleMap = () => new Promise((resolve, reject) => {
+  fs.readFile('./article-map.json', 'utf8', (error, jsonString) => (
+    error !== null ? reject(error) : resolve(JSON.parse(jsonString))
+  ))
+})
 
+router.get('/luck/article/list', async (ctx, next) => {
   try {
-    const articleMap = JSON.parse(await service())
+    const articleMap = await fetchArticleMap()
     const data = Object.keys(articleMap).map(id => articleMap[id])
     ctx.body = {
       status: true,
@@ -39,6 +39,24 @@ router.get('/luck/article/list', async (ctx, next) => {
     ctx.body = {
       status: false,
       data: [],
+      message: '文章映射列表读取失败'
+    }
+  }
+})
+
+router.get('/luck/article/detail/:id', async (ctx, next) => {
+  try {
+    const articleMap = await fetchArticleMap()
+    const id = ctx.params.id
+    const article = articleMap[id]
+    ctx.body = article !== undefined
+      ? { status: true, data: article, message: '请求成功' }
+      : { status: false, data: {}, message: '文章详情读取失败' }
+  }
+  catch {
+    ctx.body = {
+      status: false,
+      data: {},
       message: '文章映射列表读取失败'
     }
   }
